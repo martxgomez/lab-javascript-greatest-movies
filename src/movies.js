ad2ba46dafcfe72f3d0ba4c0fafe5b137066fc4f
Marta Gómez <martagomezmartinez95@gmail.com>
2024-10-06 12:54:56 +0200

    Solved iteration 3 finally

--- src/movies.js
+++ src/movies.js
@@ -1,3 +1,38 @@
+// const movies1 = [
+//   {
+//     title: "The Shawshank Redemption",
+//     year: 1994,
+//     director: "Frank Darabont",
+//     duration: "2h 22min",
+//     genre: ["Crime", "Drama"],
+//     score: "",
+//   },
+//   {
+//     title: "The Godfather",
+//     year: 1972,
+//     director: "Francis Ford Coppola",
+//     duration: "2h 55min",
+//     genre: ["Crime", "Drama"],
+//     score: 9.2,
+//   },
+//   {
+//     title: "The Godfather: Part II",
+//     year: 1974,
+//     director: "Francis Ford Coppola",
+//     duration: "3h 22min",
+//     genre: ["Crime", "Drama"],
+//     score: 9,
+//   },
+//   {
+//     title: "The Dark Knight",
+//     year: 2008,
+//     director: "Christopher Nolan",
+//     duration: "2h 32min",
+//     genre: ["Action", "Crime", "Drama", "Thriller"],
+//     score: 9,
+//   },
+// ];
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
@@ -18,62 +53,39 @@ function howManyMovies(moviesArray) {
   return SpielbergDramaMovies.length;
 }
 
-const movies1 = [
-  {
-    title: "The Shawshank Redemption",
-    year: 1994,
-    director: "Frank Darabont",
-    duration: "2h 22min",
-    genre: ["Crime", "Drama"],
-    score: ""
-  },
-  {
-    title: "The Godfather",
-    year: 1972,
-    director: "Francis Ford Coppola",
-    duration: "2h 55min",
-    genre: ["Crime", "Drama"],
-    score: 9.2,
-  },
-  {
-    title: "The Godfather: Part II",
-    year: 1974,
-    director: "Francis Ford Coppola",
-    duration: "3h 22min",
-    genre: ["Crime", "Drama"],
-    score: 9,
-  },
-  {
-    title: "The Dark Knight",
-    year: 2008,
-    director: "Christopher Nolan",
-    duration: "2h 32min",
-    genre: ["Action", "Crime", "Drama", "Thriller"],
-    score: 9,
-  },
-];
-
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {
   if (moviesArray.length === 0) {
     return 0;
   }
-  const filteredMoviesScore = moviesArray.filter((movie) => {
-    return movie.score;
-  });
-  const totalScore = filteredMoviesScore.reduce((acc, cur) => {
+
+  const totalScore = moviesArray.reduce((acc, cur) => {
+    if (!cur.score || cur.score === "") {
+      cur.score = 0;
+    }
     return acc + cur.score;
   }, 0);
 
-  const scoreAverage = totalScore / filteredMoviesScore.length;
+  const scoreAverage = totalScore / moviesArray.length;
   const scoreFixered = Number(scoreAverage.toFixed(2));
-  console.log(scoreAverage);
-  console.log(scoreFixered);
 
   return scoreFixered;
 }
 
-scoresAverage(movies1);
+// Option if movie score don't score for scoreAverage when is not defined
+//   const filteredMoviesScore = moviesArray.filter((movie) => {
+//     return movie.score;
+//   });
+//   const totalScore = filteredMoviesScore.reduce((acc, cur) => {
+//     return acc + cur.score;
+//   }, 0);
+
+//   const scoreAverage = totalScore / filteredMoviesScore.length;
+//   const scoreFixered = Number(scoreAverage.toFixed(2));
+//   console.log(scoreAverage);
+//   console.log(scoreFixered);
+
+//   return scoreFixered;
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {
@@ -86,18 +98,12 @@ function dramaMoviesScore(moviesArray) {
   return dramaMoviesAverage;
 }
 
-
-
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {
-  const moviesYear = moviesArray.toSorted((a, b)=> a.year - b.year);
+  const moviesYear = moviesArray.toSorted((a, b) => a.year - b.year);
 
-return moviesYear;
-    
+  return moviesYear;
 }
-
-console.log(orderByYear(movies1));
-
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {}
